b0fabf78f6080840a355506972c4d83f63af5058
coprum96 <medn@list.ru>
2023-03-02 18:02:16 +0300

    new

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
- 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server); // shorter syntax
+
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
- 
-http.listen(3000, function(){
-    console.log('HTTP server started on port 3000');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/front/index.html');
 });
- 
-io.on('connection', function(socket){
-    console.log('Client connection received');
+
+const connections = []; // use const instead of global variable
+
+io.on('connection', (socket) => {
+  console.log('New connection:', socket.id);
+
+  connections.push(socket);
+
+  socket.on('disconnect', () => {
+    const index = connections.indexOf(socket);
+    if (index !== -1) {
+      connections.splice(index, 1);
+      console.log('Disconnected:', socket.id);
+    }
+  });
+
+  socket.on('send mess', (data) => {
+    io.emit('add mess', { mess: data.mess, name: data.name, className: data.className });
+  });
 });
